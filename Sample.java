ad022ceabdc264b2a27a78f7175c5fc9bb04af33
RagaviAndal <andal.1902010@srit.org>
2024-06-05 13:05:48 +0530

    fixed the bug

---
+++ Sample.java
@@ -0,0 +1,9 @@
+Class Sample{
+    public static void main(String[] args){
+        System.out.println("Hello World!....");
+        System.out.println("Welcome......");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
+    }
+}
