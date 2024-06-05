a813a2a3ea1dd4fc38b8daefe2e425fb4283d02d
RagaviAndal <140416251+RagaviAndal@users.noreply.github.com>
2024-06-05 13:09:09 +0530

    feat:Added new lines

--- Sample.java
+++ Sample.java
@@ -1,9 +1,18 @@
+import java.util.*;
 Class Sample{
     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello World!....");
         System.out.println("Welcome......");
         int a = 10;
         int b = 20;
         System.out.println(a + b);
+        int age = sc.nextInt();
+        if(age > 18){
+            System.out.println("You are Eligible for vote");
+        }
+        else{
+            System.out.println("You are not eligible for vote");
+        }    
     }
 }
